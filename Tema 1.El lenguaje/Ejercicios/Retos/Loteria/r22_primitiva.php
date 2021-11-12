ed38a9e5a2fc32759a89d7b89ac8e6634f9a0775
SeniorCarlosLeal <90749875+SeniorCarlosLeal@users.noreply.github.com>
2021-11-12 16:30:49 +0100

    Add files via upload

---
+++ Tema 1.El lenguaje/Ejercicios/Retos/Loteria/r22_primitiva.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Primitiva HTML</title>
+</head>
+<body>
+	<center>
+	<img src="Imagenes/primitiva.jpg">
+	<form action="primitiva.php" method="POST">
+	<p>Fecha del sorteo <input type='date' name='fechasorteo' size=15><br>
+	<p>Recaudación Sorteo <input type='text' name='recaudacion' size=10><br>
+		<p>Pulsa para generar combinación ganadora: <input type="submit" value="Combinacion" name="combinacion"></p>
+	</form>
+</center>
+</body>
+</html>
