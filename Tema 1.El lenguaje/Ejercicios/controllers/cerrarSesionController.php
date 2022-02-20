158331a19c1da375020bc21e65d376e9b789efc8
SeniorCarlosLeal <90749875+SeniorCarlosLeal@users.noreply.github.com>
2022-02-20 09:13:07 +0100

    Delete Tema 1.El lenguaje/Ejercicios/controllers directory

--- Tema 1.El lenguaje/Ejercicios/controllers/cerrarSesionController.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("empleado","",time() - 3600,"/");
-setcookie("carrito","",time() - 3600,"/");
-header("location: ../index.php");
-?>
--- Tema 1.El lenguaje/Ejercicios/controllers/menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_COOKIE['empleado'])){
-  require_once("../views/menuVista.phtml");
-}//de if
-else{
-  header("location: ./loginController.php");
-}//de else
-
- ?>
